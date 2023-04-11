b0b1ce190ed0a1d24149bc1b144089c06d57b610
unknown <leonardohltp@gmail.com>
2023-04-10 21:23:29 -0300

    dlfdslflkdf

---
+++ exercício6.py
@@ -0,0 +1,12 @@
+alunos = {}
+for i in range(3):
+    nome = input("Digite o nome do aluno: ")
+    nota = float(input("Digite a nota do aluno: "))
+    alunos[nome] = nota
+soma = 0
+for nota in alunos.values():
+    soma += nota
+media = soma / len(alunos)
+print("A média das notas dos alunos é:", media)
+for nome, nota in alunos.items():
+    print(f"O aluno {nome} tirou a nota {nota}")
